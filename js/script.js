9a10266ae7ecabe43d3e83e4fe437dd6b594d96f
aluppan <dlmgreatb@gmail.com>
2023-04-30 02:28:17 +0400

    #3 commit

--- js/script.js
+++ js/script.js
@@ -27,10 +27,65 @@ function init() {
 
     var placemark = new ymaps.Placemark([48.872185, 2.354224], {}, {
         iconLayout: 'default#image',
-        iconImageHref: '/location.svg',
+        iconImageHref: '/img/location.svg',
         iconImageSize: [48, 48],
         iconImageOffset: [-20, -40]
     });
 
     myMap.geoObjects.add(placemark);
 }
+
+/* InputMask */
+var selector = document.querySelector("input[type='tel']");
+var im = new Inputmask("+7 (999) 999-99-99");
+
+im.mask(selector);
+
+/* Валидация */
+let validation = new JustValidate('#form', {
+    errorLabelStyle: {
+        color: '#FF5C00'
+    }
+})
+
+validation
+    .addField("#name", [
+        {
+            rule: 'required',
+            errorMessage: 'Вы не ввели имя'
+        },
+        {
+            rule: 'minLength',
+            value: 3,
+            errorMessage: 'Минимум 2 символа'
+        },
+        {
+            rule: 'maxLength',
+            value: 20,
+            errorMessage: 'Максимум 20 символа'
+        },
+    ])
+    .addField("#email", [
+        {
+            rule: 'required',
+            errorMessage: 'Вы не ввели e-mail'
+        },
+        {
+            rule: 'email',
+            errorMessage: 'Ошибка в e-mail'
+        },
+    ])
+    .addField("#phone", [
+        {
+            validator: (value) => {
+                const phone = selector.inputmask.unmaskedvalue()
+                return Boolean(Number(phone) && phone.length === 10)
+            },
+            errorMessage: 'Вы не ввели телефон'
+        }
+    ]);
+
+/* Тултип */
+tippy('[data-tippy-content]', {
+    maxWidth: 163
+});
